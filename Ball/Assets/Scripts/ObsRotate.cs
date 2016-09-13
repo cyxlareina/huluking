db450b50cbb00941fcd6846777afa87e218e862b
Feng Wang <wang167@usc.edu>
2016-09-12 22:48:28 -0700

    proto type of our game's object control

---
+++ Ball/Assets/Scripts/ObsRotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObsRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Time.deltaTime*20*Vector3.forward);
+	}
+}
