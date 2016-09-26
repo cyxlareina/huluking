6c5905424f8bba47c5b6e1db942027ec9ccd0c7d
cyxlareina <cyxlareina@gmail.com>
2016-09-26 15:06:46 -0700

    add mission4

---
+++ Huluking Game/Assets/Scripts/Scripts_MIssion4/purpleSkill.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+public class purpleSkill : MonoBehaviour {
+	public GameObject player;
+	public GameObject bullet;
+
+	private Image skillImg;
+	private bool flag;
+	// Use this for initialization
+	private void Start() {
+		flag = true;
+		skillImg = GetComponent<Image> ();
+	}
+
+	void PurpleSkill()
+	{
+		Debug.Log ("adf");
+		//Camera cam = GetComponentInChildren<Camera> ();
+		Vector3 position = player.transform.position;
+		if (flag) {
+			Instantiate(bullet, player.transform.position, player.transform.rotation);
+		}
+	}
+
+	public virtual void OnPointerDown(PointerEventData ped)
+	{
+		Vector2 pos;
+		if (RectTransformUtility.ScreenPointToLocalPointInRectangle (skillImg.rectTransform, ped.position, ped.pressEventCamera, out pos)) {
+			PurpleSkill ();
+		}
+
+	}
+
+	void Update(){
+		if (Input.GetKey (KeyCode.Space)) {
+			PurpleSkill ();
+		}
+	}
+}
