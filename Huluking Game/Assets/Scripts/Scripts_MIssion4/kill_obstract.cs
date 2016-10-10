4de44ecbf193ddd6da63c3a6387d7787271de2be
cyxlareina <cyxlareina@gmail.com>
2016-10-09 20:48:33 -0700

    update mission4
    finish

--- Huluking Game/Assets/Scripts/Scripts_MIssion4/kill_obstract.cs
+++ Huluking Game/Assets/Scripts/Scripts_MIssion4/kill_obstract.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class kill_obstract : MonoBehaviour {
 	public int life;
+	private int max_life;
+	private float init_blood_bar_x;
+	private float init_blood_bar_y;
+	public GameObject blood_bar;
 	// Use this for initialization
 	void Start () {
 		if(life == 0)
 			life = 4;
+		max_life = life;
+		init_blood_bar_x = blood_bar.transform.localScale.x;
+		init_blood_bar_y = blood_bar.transform.localScale.y;
 	}
 	
 	// Update is called once per frame
@@ -20,6 +27,8 @@ public class kill_obstract : MonoBehaviour {
 		if (other.gameObject.tag == "bullet") {
 			Destroy (other.gameObject);
 			life--;
+
+			blood_bar.transform.localScale = new Vector3(init_blood_bar_x * ((float)life / (float)max_life),init_blood_bar_y,1.0f);
 		}
 	}
 }
